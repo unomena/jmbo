be14a6cd9a9b81ac19d38c05379ca596b648edd3
Euan Jonker <euan@unomena.com>
2012-10-11 15:11:04 +0200

    Fixed the automatic publishing and reverting for ModelBase objects.

--- jmbo/managers.py
+++ jmbo/managers.py
@@ -1,12 +1,18 @@
+import datetime
+
 from django.conf import settings
 from django.db import models
 
-
 class PermittedManager(models.Manager):
     def get_query_set(self):
+        
+        now = datetime.datetime.now()
+        
         # Get base queryset and exclude based on state.
         queryset = super(PermittedManager, self).get_query_set().exclude(
-            state='unpublished'
+            state='unpublished',
+            publish_on__gt=now,
+            retract_on__lt=now
         )
 
         # Exclude objects in staging state if not in
